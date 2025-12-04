b448a9aef87c8f2080e0d946709cd9f0b2fa8187
Hoken <lol.kinx14.10.2007@gmail.com>
2025-12-04 20:53:23 +0100

    tqt

--- questionnaire.js
+++ questionnaire.js
@@ -1,16 +1,19 @@
 let ready_htpp_request = false;
+let xml_doc = load_xml_file();
 //let xml_doc = new XMLDocument();
 
 function sport_pratique() {
   if (document.getElementById("box_pratique_sport").elements[0].checked) {
     let element_to_show = document.getElementById("q_sport_pratique");
     element_to_show.hidden = false;
-    load_xml_file();
-    /*
-    for (xml_question in data.getElementByTagName("sport_pratique")) {
-      element_to_show.appendChild(document.createElement("p"));
-      }
-    */
+
+    for (xml_question in xml_doc.getElementByTagName("sport_pratique")) {
+      var option = document.createElement("option", {
+        value: xml_question.getAttribute("id"),
+      });
+
+      element_to_show.appendChild(document.createElement(option));
+    }
   } else {
     document.getElementById("q_sport_pratique").hidden = true;
   }
@@ -23,9 +26,10 @@ function load_xml_file() {
       // j'ai un truc
       ready_htpp_request = true;
     }
-    xmlhttp.open("GET", "https://louarn125.github.io/questions.xml", true);
-    xmlhttp.send();
-    let xml_doc = xmlhttp.responseXML;
-    console.log(xml_doc);
   };
+  xmlhttp.open("GET", "https://louarn125.github.io/questions.xml", true);
+  xmlhttp.send();
+  let xml_doc = xmlhttp.responseXML;
+  console.log(xml_doc);
+  return xml_doc;
 }
