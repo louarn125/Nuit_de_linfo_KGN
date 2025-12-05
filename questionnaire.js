bfcfca15d2f1fa260cc19d0eabdfa4bf43b871dc
Hoken <lol.kinx14.10.2007@gmail.com>
2025-12-05 02:12:46 +0100

    renew

--- questionnaire.js
+++ questionnaire.js
@@ -1,30 +1,6 @@
 let ready_htpp_request = false;
 let xml_doc;
-//console.log(xml_doc);
 
-// function sport_pratique() {
-//   if (document.getElementById("box_sport_pratique").checked) {
-//     let element_to_show = document.getElementById("q_sport_pratique");
-//     element_to_show.hidden = false;
-//     for (
-//       let idx_xml_question = 0;
-//       idx_xml_question < xml_doc.getElementsByTagName("sport_pratique").length;
-//       idx_xml_question++
-//     ) {
-//       document.getElementById("select_q_sport_pratique").innerHTML =
-//         document.getElementById("select_q_sport_pratique").innerHTML +
-//         "<option id=" +
-//         idx_xml_question +
-//         ">" +
-//         xml_doc.getElementsByTagName("sport_pratique")[idx_xml_question]
-//           .textContent +
-//         " </option>";
-//       s;
-//     }
-//   } else {
-//     document.getElementById("q_sport_pratique").hidden = true;
-//   }
-// }
 function flip_flop_name(name) {
   if (document.getElementById("box_" + name).checked) {
     //console.log("q_" + name);
@@ -90,27 +66,6 @@ function dynamic_charge_all() {
   for (stored_value in localStorage) {
     if (stored_value.startsWith("_")) {
       better_stored_value = stored_value.slice(1);
-      //   console.log("in local: ", localStorage.getItem(stored_value));
-
-      //   console.log(
-      //     "in document: ",
-      //     document.getElementById(better_stored_value),
-      //   );
-      //   console.log(
-      //     "the id: ",
-      //     stored_value,
-      //     " and the better one: ",
-      //     better_stored_value,
-      //   );
-
-      // console.log(stored_value);
-      // console.log(
-      //   stored_value,
-      //   " sera chargé à ",
-      //   localStorage.getItem(stored_value),
-      //   " il était avant à ",
-      //   document.getElementById(better_stored_value).checked,
-      // );
       if (better_stored_value.startsWith("box_")) {
         document.getElementById(better_stored_value).checked =
           localStorage.getItem(stored_value) === "true";
@@ -126,4 +81,8 @@ function dynamic_charge_all() {
 
 function load_page() {
   load_xml_file();
+}
+
+function encookization() {
+  document.cookie = newCookie;
 }
